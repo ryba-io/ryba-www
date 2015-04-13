1a4294c9c53bf5bdb61864201dbbb95f4021ffa4
David Worms <david@adaltas.com>
2015-04-13 02:00:49 +0200

    remove old code

--- public/js/Documentation.cjsx
+++
@@ -1,2 +0,0 @@
-require('../css/documentation.styl')
-
--- public/js/test.js
+++
@@ -1,3 +0,0 @@
-
-// require('imports?jQuery=jquery!../vendors/semantic-ui/dist/semantic.min.js')
-// require('imports?$=jquery!imports?jQuery=jquery!imports?s=../vendors/semantic-ui/dist/semantic.min.js!./ryba.cjsx')
